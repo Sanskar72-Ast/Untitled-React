c3cb9cf1be74e022d766dedf85641b1239053435
Sanskar72-Ast <118362903+Sanskar72-Ast@users.noreply.github.com>
2024-10-23 10:08:34 +0530

    Commit from TeleportHQ

---
+++ src/views/home.js
@@ -0,0 +1,356 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet'
+
+import './home.css'
+
+const Home = (props) => {
+  return (
+    <div className="home-container">
+      <Helmet>
+        <title>exported project</title>
+      </Helmet>
+      <div className="home-home">
+        <img
+          alt="Rectangle31498"
+          src="/external/rectangle31498-2oiws-1200w.png"
+          className="home-rectangle3"
+        />
+        <span className="home-text10">
+          <span className="home-text11">yo, what&apos;s up?</span>
+          <br></br>
+          <span className="home-text13"></span>
+          <br></br>
+          <span className="home-text15">
+            so if you’ve made it here, i’m guessing you’re not just out here
+            looking for the usual. nah, you wanna do something that counts.
+            something that’s bigger than just vibing through your days, wasting
+            time scrolling through random stuff, or waiting for another boring
+            class to end.
+            <span
+              dangerouslySetInnerHTML={{
+                __html: ' ',
+              }}
+            />
+          </span>
+          <br></br>
+          <span className="home-text17"></span>
+          <br></br>
+          <span className="home-text19">
+            and honestly, that’s why you’re still reading. ‘cause maybe, just
+            maybe, there’s a little spark in you, something that’s been nudging
+            you, whispering, “hey, there’s more to this than just your everyday
+            routine. you’ve got something to offer, something to give.” maybe
+            you’re starting to feel like, “yo, i could be doing something real
+            right now,” instead of just watching life pass by, doing the same
+            thing, day in and day out. and yeah, we totally get that.
+          </span>
+          <br></br>
+          <span className="home-text21"></span>
+          <br></br>
+          <span className="home-text23">
+            that’s why
+            <span
+              dangerouslySetInnerHTML={{
+                __html: ' ',
+              }}
+            />
+          </span>
+          <span className="home-text24">stemifyy</span>
+          <span> exists.</span>
+        </span>
+        <div className="home-background1">
+          <div className="home-background2">
+            <img
+              alt="IMAGE100007737911410"
+              src="/external/image100007737911410-tkxo-200h.png"
+              className="home-image10000773791"
+            />
+          </div>
+        </div>
+        <span className="home-text26">
+          <span className="home-text27">
+            and listen, you don’t need to be a genius or have years of
+            experience to join us. it’s not about that. all we’re asking for is
+            some passion, a little bit of your time, and a mindset that’s all
+            about helping others. we’ll handle the rest together. you don’t need
+            to be perfect – you just need to show up and give a damn.
+          </span>
+          <br></br>
+          <br></br>
+          <span className="home-text30">
+            if you’re still here and nodding along, well, that’s a sign, isn’t
+            it?
+          </span>
+          <br></br>
+          <br></br>
+          <span className="home-text33">
+            so, if all of this is hitting right, and you’re feeling like you’re
+            ready to jump into something that actually matters, then go ahead
+            and
+            <span
+              dangerouslySetInnerHTML={{
+                __html: ' ',
+              }}
+            />
+          </span>
+          <span className="home-text34">click here to join us</span>
+          <span className="home-text35">
+            . let’s chat, figure out where you fit into the crew, and get
+            moving. you’ll get to meet other chill, like-minded people who are
+            in it for the same reasons. and yeah, you might just make some cool
+            friends along the way, too. i mean, that’s how our team came
+            together – a bunch of people with the same drive, and now we’re out
+            here doing something real.
+          </span>
+          <br></br>
+          <br></br>
+          <span className="home-text38">
+            but hey, if you’re still on the fence, that’s okay too. no pressure.
+            just think about this for a sec: a year from now, you could either
+            be looking back at a bunch of random days that flew by with nothing
+            major happening, or you could be saying, “yeah, i helped build
+            something that’s changing lives.” which one sounds better?
+          </span>
+          <br></br>
+          <br></br>
+          <span className="home-text41">
+            and look, we’re not saying you have to decide right now, at this
+            exact moment. take your time. mull it over. but just know, whenever
+            you’re ready, we’ll be here. we’re not going anywhere. we’ve got
+            some massive plans cooking, and trust me, you’re not gonna want to
+            miss out on being a part of it.
+          </span>
+          <br></br>
+          <br></br>
+          <span className="home-text44">
+            the world’s moving fast. don’t let it pass you by without leaving
+            your mark on it.
+          </span>
+          <br></br>
+          <span className="home-text46">
+            so, yeah. don’t sleep on this, fam.
+          </span>
+          <br></br>
+          <br></br>
+          <span className="home-text49">fill this out asap!!</span>
+          <br></br>
+          <br></br>
+          <span>
+            we’ll be here when you’re ready. let’s make something epic together.
+          </span>
+          <br></br>
+          <br></br>
+          <span>peace. ✌️</span>
+          <br></br>
+          <br></br>
+          <span>cc: crew@stemifyy.org</span>
+          <br></br>
+          <br></br>
+          <span>
+            oh btw, here’s a photo dump to keep it fresh. gotta keep the vibes
+            alive, right?
+            <span
+              dangerouslySetInnerHTML={{
+                __html: ' ',
+              }}
+            />
+          </span>
+          <br></br>
+          <br></br>
+          <span>
+            check us out on ig @stemifyy for all the cool behind-the-scenes,
+            updates, and whatever randomness we get up to. trust me, you
+            don&apos;t wanna miss out.
+          </span>
+          <br></br>
+          <br></br>
+          <span>
+            and fr, don’t sleep on the join us button – let’s actually build
+            something that matters. no cap.
+          </span>
+          <br></br>
+          <br></br>
+          <span>stay lit 🔥</span>
+        </span>
+        <img
+          alt="IMG20240609WA00291410"
+          src="/external/img20240609wa00291410-hijd-300w.png"
+          className="home-img20240609wa0029"
+        />
+        <img
+          alt="IMG20240609WA00181410"
+          src="/external/img20240609wa00181410-qs4l-800w.png"
+          className="home-img20240609wa0018"
+        />
+        <img
+          alt="IMG20240612WA00141410"
+          src="/external/img20240612wa00141410-lfer-300w.png"
+          className="home-img20240612wa0014"
+        />
+        <img
+          alt="IMG20240612WA00061411"
+          src="/external/img20240612wa00061411-sqrp-500w.png"
+          className="home-img20240612wa0006"
+        />
+        <img
+          alt="IMG20240609WA002511411"
+          src="/external/img20240609wa002511411-u9w-300w.png"
+          className="home-img20240609wa00251"
+        />
+        <img
+          alt="IMG20240612WA000511411"
+          src="/external/img20240612wa000511411-4k15-400w.png"
+          className="home-img20240612wa00051"
+        />
+        <img
+          alt="IMG20240609WA00281411"
+          src="/external/img20240609wa00281411-9bt-400w.png"
+          className="home-img20240609wa0028"
+        />
+        <img
+          alt="IMG20240612WA00201411"
+          src="/external/img20240612wa00201411-js3h-300h.png"
+          className="home-img20240612wa0020"
+        />
+        <img
+          alt="IMG20240612WA000411411"
+          src="/external/img20240612wa000411411-k5s-200h.png"
+          className="home-img20240612wa00041"
+        />
+        <img
+          alt="IMG20240609WA002611411"
+          src="/external/img20240609wa002611411-ny8d-300w.png"
+          className="home-img20240609wa00261"
+        />
+        <img
+          alt="IMG20240609WA002011411"
+          src="/external/img20240609wa002011411-nj8l-700w.png"
+          className="home-img20240609wa00201"
+        />
+        <img
+          alt="WhatsAppImage20241006at132021e606fd2e11412"
+          src="/external/whatsappimage20241006at132021e606fd2e11412-d2kf-400h.png"
+          className="home-whats-app-image20241006at132021e606fd2e1"
+        />
+        <img
+          alt="IMG20240612WA001511412"
+          src="/external/img20240612wa001511412-0tsr-400w.png"
+          className="home-img20240612wa00151"
+        />
+        <img
+          alt="IMG20240612WA001521412"
+          src="/external/img20240612wa001521412-bw0v-400w.png"
+          className="home-img20240612wa00152"
+        />
+        <img
+          alt="IMG20240612WA00031411"
+          src="/external/img20240612wa00031411-i2a-400w.png"
+          className="home-img20240612wa0003"
+        />
+        <span className="home-text71">
+          <span className="home-text72">
+            we’re here to do the stuff that actually matters. no bs, no fluff –
+            just straight-up impact. you know the deal – the world is built on
+            STEM. coding, AI, robotics, engineering – all those things that are
+            literally shaping the future right now. but let’s be real for a sec:
+            not everyone gets to be a part of that. some people, just because of
+            where they’re born, the school they go to, or what resources they’ve
+            got access to – they don’t get a fair shot at all that future stuff.
+            and if you think that’s messed up, you’re not alone. we think it’s
+            pretty broken too.
+          </span>
+          <br></br>
+          <span className="home-text74"></span>
+          <br></br>
+          <span className="home-text76">
+            now, we could all just sit back, pretend it&apos;s not happening,
+            and dive deeper into our own worlds. like, we could be out here
+            binging on netflix or endlessly swiping on tiktok like zombies. but
+            that’s not what we’re about. if you’re still here, we’re betting
+            you’re not about that either. you’re here because you want to do
+            something. and guess what? so do we.
+          </span>
+          <br></br>
+          <span className="home-text78"></span>
+          <br></br>
+          <span className="home-text80">
+            that’s the core of stemifyy. we’re a crew of people who believe that
+            STEM isn’t just for a select few – it’s for everyone. whether you’re
+            living in the heart of some bustling city or you’re from a small,
+            remote village that doesn’t even show up on google maps, you should
+            have access to the same stuff: tools to learn, the chance to create,
+            the opportunity to innovate. ‘cause in the end, it’s people like us,
+            the next generation, who are gonna be pushing the world forward.
+          </span>
+          <br></br>
+          <span className="home-text82"></span>
+          <br></br>
+          <span className="home-text84">
+            we’re not some mega-corporation or a team of old dudes in suits.
+            nah, we’re students, dreamers, nerds, and creators who are just
+            trying to make this world a bit fairer. people like
+            <span
+              dangerouslySetInnerHTML={{
+                __html: ' ',
+              }}
+            />
+          </span>
+          <span className="home-text85">
+            sanskar, atharv, aditya, aishani, aishnika, shreya
+          </span>
+          <span>
+            {' '}
+            and the whole crew – we’ve been there, grinding through the system,
+            realizing that something needs to change. so we decided to stop
+            talking and actually do something about it. and here we are, making
+            moves, setting up workshops, creating cool content, and spreading
+            the word like wildfire. but you know what? we’re still a small team.
+            and there’s only so much we can do alone.
+          </span>
+          <br></br>
+          <span></span>
+          <br></br>
+          <span>this is where you come in.</span>
+          <br></br>
+          <span></span>
+          <br></br>
+          <span>
+            we’re not gonna sit here and tell you to “volunteer” like it’s some
+            boring task that your parents made you do. nah, this is different.
+            this is about stepping up and actually making a dent in something
+            real. it’s about being part of something that, when you look back a
+            few months or a year from now, you’ll feel proud of. not just some
+            ticked-off box on your resume. nah, it’s more like building
+            something that actually sticks around. imagine this: there’s a kid
+            out there who’s never even touched a computer, and now they’re
+            figuring out how to code because you were a part of this. that’s the
+            kind of change we’re talking about.
+          </span>
+        </span>
+        <img
+          alt="IMG20240628WA00011410"
+          src="/external/img20240628wa00011410-bhwa-800w.png"
+          className="home-img20240628wa0001"
+        />
+        <img
+          alt="IMG20240612WA001611411"
+          src="/external/img20240612wa001611411-6v29-400h.png"
+          className="home-img20240612wa00161"
+        />
+        <img
+          alt="IMG20240609WA00191410"
+          src="/external/img20240609wa00191410-wktc-800w.png"
+          className="home-img20240609wa0019"
+        />
+        <img
+          alt="IMG20240609WA002711493"
+          src="/external/img20240609wa002711493-yqlk-200w.png"
+          className="home-img20240609wa00271"
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Home
